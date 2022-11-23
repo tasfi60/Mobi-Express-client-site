d9f0cd3a8bc1f55ebc690ef78278843e516fd89d
tasfi60 <tasfiagirl@gmail.com>
2022-11-24 02:16:24 +0600

    404

--- src/AllRoutes/Routes/Routes.js
+++ src/AllRoutes/Routes/Routes.js
@@ -1,9 +1,12 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter,Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import './Routes.css';
 import Main from "../../Layout/Main";
 import Home from "../../Allpages/Home/Home/Home";
 import Blog from "../../Allpages/Blog/Blog";
 import Login from "../../Allpages/Login/Login";
 import Register from "../../Allpages/Register/Register";
+
 
 export const routes = createBrowserRouter([
     {
@@ -30,6 +33,40 @@ export const routes = createBrowserRouter([
     },
 
     { path: '*',
-    element: <div><h2 className='mt-5'>404 ! This route is not found</h2></div> 
+    element: <div>
+
+                   
+<div className=" main-div mb-5">
+	
+	<div  className="error">
+		<p className="p">4</p>
+		<span className="dracula">			
+			<div className="con">
+				<div className="hair"></div>
+				<div className="hair-r"></div>
+				<div className="head"></div>
+    		<div className="eye"></div>
+    		<div className="eye eye-r"></div>
+  			<div className="mouth"></div>
+  			<div className="blod"></div>
+  			<div className="blod blod2"></div>
+			</div>
+		</span>
+		<p className="p">4</p>
+		
+		<div className="page-ms">
+			<p className="page-msg"> Oops, You're lost! </p>
+			<button className="go-back"><Link className='btn' to="/">Go Back</Link></button>
+		</div>
+</div>
+	</div>
+
+
+          
+         
+
+       
+        
+            </div> 
    }
 ]);
